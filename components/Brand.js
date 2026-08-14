a9b6e02d422bc615cc9c19877db885a7a0816509
sasmithconsult <sasmithconsult@gmail.com>
2026-08-13 20:21:47 -0700

    Use theme-aware Akhada header marks

--- components/Brand.js
+++ components/Brand.js
@@ -1,15 +1,14 @@
 import Link from "next/link";
 
-export default function Brand() {
+export default function Brand({ light = false }) {
+  const markSrc = light
+    ? "/images/akhada-mark-dark.png"
+    : "/images/akhada-mark-light.png";
+
   return (
     <Link className="brand" href="/" aria-label="Akhada Consulting home">
       <span className="brand-mark" aria-hidden="true">
-        <img
-          src="/images/akhada-logo.png"
-          alt=""
-          width="325"
-          height="385"
-        />
+        <img src={markSrc} alt="" width="325" height="385" />
       </span>
       <span className="brand-wordmark">
         AKHADA <small>CONSULTING</small>
