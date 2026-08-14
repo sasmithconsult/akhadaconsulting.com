4d0d4b2580651990ebb821602bcfab44767cf457
sasmithconsult <sasmithconsult@gmail.com>
2026-08-13 19:44:46 -0700

    Restore Akhada mark to site branding

--- components/Brand.js
+++ components/Brand.js
@@ -3,6 +3,14 @@ import Link from "next/link";
 export default function Brand() {
   return (
     <Link className="brand" href="/" aria-label="Akhada Consulting home">
+      <span className="brand-mark" aria-hidden="true">
+        <img
+          src="/images/akhada-logo.png"
+          alt=""
+          width="325"
+          height="385"
+        />
+      </span>
       <span className="brand-wordmark">
         AKHADA <small>CONSULTING</small>
       </span>
